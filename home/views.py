9768bd81f4cc36633d66cea07b4ba76586b7570f
Arun Dhanjal <arundhanjal@hotmail.com>
2026-01-06 20:52:08 +0000

    Create homepage for django practice project to display buttons that will redirect to other django apps

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
 
 
-def home_page_view(request):
-    return HttpResponse("This is the home page!")
+def homepage(request):
+    apps = [
+        {"name": "Blog", "url": "/blog/"},
+        {"name": "Shop", "url": "/shop/"},
+        {"name": "Tasks", "url": "/tasks/"},
+    ]
+    return render(request, "home/home.html", {"apps": apps})
